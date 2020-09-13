ac3ac3ddff777ad4fd0ebcf9243137891fe29c0f
Elias Naur <mail@eliasnaur.com>
2020-09-13 12:58:47 +0200

    shader: introduce a crude setting for adjusting the maximum workgroup size
    Both the Vulkan and OpenGL ES spec allow implementations to limit workgroups to
    128 threads. Add a LG_WG_FACTOR setting for easy switching between 128 and 256
    threads, with 256 being kept as the default setting.
    
    Manually tested that LG_WG_FACTOR = 0 (128 threads) works as expected.
    
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- piet-gpu/shader/setup.h
+++ piet-gpu/shader/setup.h
@@ -3,6 +3,11 @@
 // Much of this will be made dynamic in various ways, but for now it's easiest
 // to hardcode and keep all in one place.
 
+// A LG_WG_FACTOR of n scales workgroup sizes by 2^n. Use 0 for a
+// maximum workgroup size of 128, or 1 for a maximum size of 256.
+#define LG_WG_FACTOR 1
+#define WG_FACTOR (1<<LG_WG_FACTOR)
+
 // TODO: compute all these
 
 #define WIDTH_IN_TILES 128
@@ -17,7 +22,7 @@
 // of that kernel is equal to the number of bins, but should probably
 // be more flexible (it's 512 in the K&L paper).
 #define N_TILE_X 16
-#define N_TILE_Y 16
+#define N_TILE_Y (8 * WG_FACTOR)
 #define N_TILE (N_TILE_X * N_TILE_Y)
-#define LG_N_TILE 8
+#define LG_N_TILE (7 + LG_WG_FACTOR)
 #define N_SLICE (N_TILE / 32)
