44bff2726cc844fcb4c370a76bf2604a1f48d7bc
Elias Naur <mail@eliasnaur.com>
2021-03-17 16:34:21 +0100

    collapse FillCubic and StrokeCubic into Cubic with flags for fill mode
    Updates #70
    
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- piet-gpu-types/src/pathseg.rs
+++ piet-gpu-types/src/pathseg.rs
@@ -3,32 +3,20 @@ use piet_gpu_derive::piet_gpu;
 piet_gpu! {
     #[gpu_write]
     mod pathseg {
-        struct PathFillCubic {
+        struct PathCubic {
             p0: [f32; 2],
             p1: [f32; 2],
             p2: [f32; 2],
             p3: [f32; 2],
             path_ix: u32,
-            // trans_ix is 1-based, 0 means no transformation.
+            // trans_ix is the transform index. It is 1-based, 0 means no transformation.
             trans_ix: u32,
-            // A note: the layout of this struct is shared with
-            // PathStrokeCubic. In that case, we actually write
-            // [0.0, 0.0] as the stroke field, to minimize divergence.
-        }
-        struct PathStrokeCubic {
-            p0: [f32; 2],
-            p1: [f32; 2],
-            p2: [f32; 2],
-            p3: [f32; 2],
-            path_ix: u32,
-            trans_ix: u32,
-            // halfwidth in both x and y for binning
+            // Halfwidth in both x and y for binning. For strokes only.
             stroke: [f32; 2],
         }
         enum PathSeg {
             Nop,
-            FillCubic(PathFillCubic),
-            StrokeCubic(PathStrokeCubic),
+            Cubic(TagFlags, PathCubic),
         }
     }
 }
