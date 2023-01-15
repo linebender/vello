519da677eb63cebad53d241676ed5ed9085b9a97
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2023-01-15 14:01:08 +0000

    Update to newer than newest cargo-run-wasm
    Add some doc comments

--- examples/run_wasm/src/main.rs
+++ examples/run_wasm/src/main.rs
@@ -1,4 +1,17 @@
+/// Use [cargo-run-wasm](https://github.com/rukai/cargo-run-wasm) to build an example for web
+///
+/// Usage:
+/// ```
+/// cargo run -p run_wasm -- --package [example_name]
+/// ```
+/// Generally:
+/// ```
+/// cargo run -p run_wasm -- --package with_winit
+/// ```
+
 fn main() {
+    // HACK: We rely heavily on compute shaders; which means we need WebGPU to be supported
+    // However, that requires unstable APIs to be enabled, which are not exposed through a feature
     let current_value = std::env::var("RUSTFLAGS").unwrap_or("".to_owned());
     std::env::set_var(
         "RUSTFLAGS",
