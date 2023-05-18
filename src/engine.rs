1f6b47bd78b7714a7c5fd585cdbc0c9f99e7070a
Raph Levien <raph@google.com>
2023-05-18 15:36:06 -0700

    Quiet warnings
    Fixes the warnings remaining in the code, which should in turn let us check that in CI.
    
    Also checks the optional features, which are easy enough to break.

--- src/engine.rs
+++ src/engine.rs
@@ -63,6 +63,7 @@ pub struct BufProxy {
 #[derive(Copy, Clone, PartialEq, Eq)]
 pub enum ImageFormat {
     Rgba8,
+    #[allow(unused)]
     Bgra8,
 }
 
@@ -81,6 +82,7 @@ pub enum ResourceProxy {
 }
 
 pub enum ExternalResource<'a> {
+    #[allow(unused)]
     Buf(BufProxy, &'a Buffer),
     Image(ImageProxy, &'a TextureView),
 }
@@ -800,7 +802,7 @@ impl ResourcePool {
     fn get_buf(
         &mut self,
         size: u64,
-        name: &'static str,
+        #[allow(unused)] name: &'static str,
         usage: BufferUsages,
         device: &Device,
     ) -> Buffer {
