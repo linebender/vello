12c60569e1cee3f62196e097566b051ba5d67182
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2024-02-14 17:15:37 +0000

    build script: rerun properly

--- crates/tests/build.rs
+++ crates/tests/build.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 fn main() {
+    println!("cargo:rerun-if-env-changed=VELLO_CI_NO_GPU");
     if env::var("VELLO_CI_NO_GPU").is_ok() {
         println!("cargo:rustc-cfg=skip_gpu_tests");
     }
