4951a7c4e64fbff237fdc116836b1a7fd2169bc1
Kaur Kuut <strom@nevermore.ee>
2024-03-06 22:55:51 +0200

    Be implicit in targeting `--lib` & `--bins` to prevent an error.

--- examples/simple/src/lib.rs
+++
@@ -1,5 +0,0 @@
-// Copyright 2024 the Vello Authors
-// SPDX-License-Identifier: Apache-2.0 OR MIT
-
-// This empty file is a hack and can be safely ignored.
-// Running Clippy with --lib will error out unless we have at least this empty file.
