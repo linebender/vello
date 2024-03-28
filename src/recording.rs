fa3394b8d1bf0d4ef5ecdb238e6d595fa404adb0
Lixou <82600264+DasLixou@users.noreply.github.com>
2024-03-28 17:40:26 +0100

    Update recording.rs

--- src/recording.rs
+++ src/recording.rs
@@ -15,8 +15,8 @@ impl ResourceId {
         static ID_COUNTER: AtomicU64 = AtomicU64::new(1);
 
         let val = ID_COUNTER.fetch_add(1, Ordering::Relaxed);
-        // SAFETY: Smallest value is 1.
-        ResourceId(unsafe { NonZeroU64::new_unchecked(val + 1) })
+        // SAFETY: We begin with 1.
+        ResourceId(unsafe { NonZeroU64::new_unchecked(val) })
     }
 }
 
