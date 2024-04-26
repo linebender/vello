fb4154a7390ecfc000d9fb492ce4cc802b862c96
Arman Uguray <armansito@google.com>
2024-04-26 13:08:09 -0700

    Scene::print_path_counts

--- examples/with_winit/src/lib.rs
+++ examples/with_winit/src/lib.rs
@@ -199,6 +199,7 @@ fn run(
 
     #[cfg(feature = "debug_layers")]
     let mut debug = vello::DebugLayers::none();
+    let mut debug_dump_time = Instant::now();
 
     #[allow(deprecated)]
     event_loop
@@ -563,6 +564,23 @@ fn run(
                             frame_time_us: (new_time - frame_start_time).as_micros() as u64,
                         });
                         frame_start_time = new_time;
+
+                        if (new_time - debug_dump_time).as_secs() > 2 && scene_complexity.is_some() {
+                            scene.print_path_counts();
+                            //let bump_estimate = scene.bump_estimate(None);
+                            let bump_actual = scene_complexity.as_ref().unwrap();
+                            //println!("Last frame estimated bump buffer counts:{bump_estimate}\n");
+                            println!(
+                                "Last frame actual bump buffer counts:{}\n \
+                                    \tBlend:\t\t\t{}\n\
+                                    \tError Bits:\t\t0x{:#08x}\n\
+                                    --------\n",
+                                    bump_actual.memory(),
+                                    bump_actual.blend,
+                                    bump_actual.failed
+                            );
+                            debug_dump_time = new_time;
+                        }
                     }
                     _ => {}
                 }
