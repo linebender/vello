5a5a638a58a524230484db110151fa7591904a88
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2024-07-16 10:56:21 +0100

    Don't create a zero sized scene buffer (#630)
    Alternative to #629
    
    Fixes #291

--- vello_tests/tests/smoke.rs
+++ vello_tests/tests/smoke.rs
@@ -6,20 +6,6 @@ use vello::peniko::{Brush, Color, Format};
 use vello::Scene;
 use vello_tests::TestParams;
 
-#[test]
-#[cfg_attr(skip_gpu_tests, ignore)]
-fn simple_square_gpu() {
-    simple_square(false);
-}
-
-#[test]
-// The fine shader still requires a GPU, and so we still get a wgpu device
-// skip this for now
-#[cfg_attr(skip_gpu_tests, ignore)]
-fn simple_square_cpu() {
-    simple_square(true);
-}
-
 fn simple_square(use_cpu: bool) {
     let mut scene = Scene::new();
     scene.fill(
@@ -53,4 +39,55 @@ fn simple_square(use_cpu: bool) {
     }
     assert_eq!(red_count, 50 * 50);
     assert_eq!(black_count, 150 * 150 - 50 * 50);
+}
+
+fn empty_scene(use_cpu: bool) {
+    let scene = Scene::new();
+
+    // Adding an alpha factor here changes the resulting colour *slightly*,
+    // presumably due to pre-multiplied alpha.
+    // We just assume that alpha scenarios work fine
+    let color = Color::PLUM;
+    let params = TestParams {
+        use_cpu,
+        base_colour: Some(color),
+        ..TestParams::new("simple_square", 150, 150)
+    };
+    let image = vello_tests::render_then_debug_sync(&scene, &params).unwrap();
+    assert_eq!(image.format, Format::Rgba8);
+    for pixel in image.data.data().chunks_exact(4) {
+        let &[r, g, b, a] = pixel else { unreachable!() };
+        let image_color = Color::rgba8(r, g, b, a);
+        if image_color != color {
+            panic!("Got {image_color:?}, expected clear colour {color:?}");
+        }
+    }
+}
+
+#[test]
+#[cfg_attr(skip_gpu_tests, ignore)]
+fn simple_square_gpu() {
+    simple_square(false);
+}
+
+#[test]
+// The fine shader still requires a GPU, and so we still get a wgpu device
+// skip this for now
+#[cfg_attr(skip_gpu_tests, ignore)]
+fn simple_square_cpu() {
+    simple_square(true);
+}
+
+#[test]
+#[cfg_attr(skip_gpu_tests, ignore)]
+fn empty_scene_gpu() {
+    empty_scene(false);
+}
+
+#[test]
+// The fine shader still requires a GPU, and so we still get a wgpu device
+// skip this for now
+#[cfg_attr(skip_gpu_tests, ignore)]
+fn empty_scene_cpu() {
+    empty_scene(true);
 }
