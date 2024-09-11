d26945bc65a19f539528c55333710c77dd407a2b
Thomas Smith <68340554+b0nes164@users.noreply.github.com>
2024-09-11 14:45:44 -0700

    Remove unused workgroup and buffer sizes.

--- vello_encoding/src/config.rs
+++ vello_encoding/src/config.rs
@@ -203,10 +203,6 @@ pub type WorkgroupSize = (u32, u32, u32);
 /// Computed sizes for all dispatches.
 #[derive(Copy, Clone, Debug, Default)]
 pub struct WorkgroupCounts {
-    pub use_large_path_scan: bool,
-    pub path_reduce: WorkgroupSize,
-    pub path_reduce2: WorkgroupSize,
-    pub path_scan1: WorkgroupSize,
     pub path_scan: WorkgroupSize,
     pub bbox_clear: WorkgroupSize,
     pub flatten: WorkgroupSize,
@@ -237,12 +233,6 @@ impl WorkgroupCounts {
         let n_clips = layout.n_clips;
         let path_tag_padded = align_up(n_path_tags, 4 * PATH_REDUCE_WG);
         let path_tag_wgs = path_tag_padded / (4 * PATH_REDUCE_WG);
-        let use_large_path_scan = path_tag_wgs > PATH_REDUCE_WG;
-        let reduced_size = if use_large_path_scan {
-            align_up(path_tag_wgs, PATH_REDUCE_WG)
-        } else {
-            path_tag_wgs
-        };
         let draw_object_wgs = (n_draw_objects + PATH_BBOX_WG - 1) / PATH_BBOX_WG;
         let draw_monoid_wgs = draw_object_wgs.min(PATH_BBOX_WG);
         let flatten_wgs = (n_path_tags + FLATTEN_WG - 1) / FLATTEN_WG;
@@ -252,10 +242,6 @@ impl WorkgroupCounts {
         let width_in_bins = (width_in_tiles + 15) / 16;
         let height_in_bins = (height_in_tiles + 15) / 16;
         Self {
-            use_large_path_scan,
-            path_reduce: (path_tag_wgs, 1, 1),
-            path_reduce2: (PATH_REDUCE_WG, 1, 1),
-            path_scan1: (reduced_size / PATH_REDUCE_WG, 1, 1),
             path_scan: (path_tag_wgs, 1, 1),
             bbox_clear: (draw_object_wgs, 1, 1),
             flatten: (flatten_wgs, 1, 1),
@@ -364,13 +350,8 @@ impl BufferSizes {
         let n_paths = layout.n_paths;
         let n_draw_objects = layout.n_draw_objects;
         let n_clips = layout.n_clips;
-        let path_tag_wgs = workgroups.path_reduce.0;
-        let reduced_size = if workgroups.use_large_path_scan {
-            align_up(path_tag_wgs, PATH_REDUCE_WG)
-        } else {
-            path_tag_wgs
-        };
-        let path_reduced = BufferSize::new(reduced_size);
+        let path_tag_wgs = workgroups.path_scan.0;
+        let path_reduced = BufferSize::new(path_tag_wgs);
         let path_scan_bump = BufferSize::new(1);
         let path_monoids = BufferSize::new(path_tag_wgs * PATH_REDUCE_WG);
         let path_bboxes = BufferSize::new(n_paths);
