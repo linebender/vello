9f0a1f181181b56cc3cb31c7bc3d264d2752730f
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2024-12-04 17:16:36 +0000

    Some first sketches of a render graph

--- vello/src/graph.rs
+++ vello/src/graph.rs
@@ -5,6 +5,89 @@
 //!
 //! This enables the use of image filters among other things.
 
+use std::{
+    collections::HashMap,
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
+};
+
+use peniko::Image;
+
+struct Generation(u32);
+
+impl Generation {}
+
+/// A partial render graph.
+pub struct Gallery {
+    id: u32,
+    generation: Generation,
+    paintings: HashMap<Painting, PaintingSource>,
+}
+
+enum PaintingSource {
+    Scene(Scene),
+    Image(Image),
+    Region {
+        painting: Painting,
+        x: u32,
+        y: u32,
+        width: u32,
+        height: u32,
+    },
+}
+
+/// An allocator of [`Painting`]s, associated with a [`Vello`].
+pub struct Curator(Arc<CuratorInner>);
+
+struct CuratorInner {
+    painting_ids: AtomicU64,
+}
+
+impl Curator {
+    pub fn painting(&self) -> Painting {
+        self.0.painting_ids.fetch_add(1, Ordering::Relaxed);
+        Painting {}
+    }
+}
+
+pub enum OutputSize {
+    Fixed { width: u32, height: u32 },
+    Inferred,
+}
+
+impl Gallery {
+    pub fn replace_image(&mut self, _texture: Painting, _image: Image) {}
+    pub fn subregion(
+        &mut self,
+        target: Painting,
+        of_: Painting,
+        x: u32,
+        y: u32,
+        width: u32,
+        height: u32,
+    ) {
+    }
+    pub fn resample(&mut self, painting: Painting, dimensions: OutputSize) -> Painting {
+        Painting {}
+    }
+}
+
+/// A single Scene
+pub struct Scene {}
+
+impl Scene {
+    #[doc(alias = "image")]
+    pub fn painting(&mut self, drawing: Painting, width: u32, height: u32) {}
+}
+
+/// A handle to an image managed by the renderer.
+///
+/// The corresponding resource is reference counted.
+#[derive(Clone)]
+pub struct Painting {}
+
 /// When making an image filter graph, we need to know a few things:
 ///
 /// 1) The Scene to draw.
@@ -15,3 +98,25 @@
 /// The resolution of the input might change depending on the resolution of the
 /// output, because of scaling/rotation/skew.
 pub struct Thinking;
+
+/// What threading model do we want. Requirements:
+/// 1) Creating scenes on different threads should be possible.
+/// 2) Scenes created on different threads should be able to use filter effects.
+/// 3) We should only upload each CPU side image once.
+///
+///
+pub struct Threading;
+
+/// Question: What do we win from backpropogating render sizes?
+/// Answer: Image sampling
+///
+/// Conclusion: Special handling of "automatic" scene sizing to
+/// render multiple times if needed.
+///
+/// Conclusion: Two phase approach, backpropogating from every scene
+/// with a defined size?
+pub struct ThinkingAgain;
+
+/// Do we want custom fully graph nodes?
+/// Answer for now: No?
+pub struct Scheduling;
