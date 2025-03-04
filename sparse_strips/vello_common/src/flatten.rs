5073663b4e8a2ac614de5bf1fd98c05406e22175
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-03-04 19:34:46 +0100

    Re-add hack for tolerance

--- sparse_strips/vello_common/src/flatten.rs
+++ sparse_strips/vello_common/src/flatten.rs
@@ -107,7 +107,10 @@ pub fn fill(path: &BezPath, affine: Affine, line_buf: &mut Vec<Line>) {
 
 /// Flatten a stroked bezier path into line segments.
 pub fn stroke(path: &BezPath, style: &Stroke, affine: Affine, line_buf: &mut Vec<Line>) {
-    let expanded = kurbo::stroke(path.iter(), style, &StrokeOpts::default(), TOL);
+    // TODO: Temporary hack to ensure that strokes are scaled properly by the transform.
+    let tolerance = TOL / affine.as_coeffs()[0].abs().max(affine.as_coeffs()[3].abs());
+
+    let expanded = kurbo::stroke(path.iter(), style, &StrokeOpts::default(), tolerance);
     fill(&expanded, affine, line_buf);
 }
 
