24a8634a6d16bb0b6606482d7df7745a8691d6e7
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-03-04 11:04:23 +0100

    Add message to assertion

--- sparse_strips/vello_common/src/wide.rs
+++ sparse_strips/vello_common/src/wide.rs
@@ -61,7 +61,7 @@ impl Wide {
     ///
     /// Panics if the index is out-of-range.
     pub fn get(&self, x: usize, y: usize) -> &WideTile {
-        assert!(x < self.width && y < self.height);
+        assert!(x < self.width && y < self.height, "attempted to access out-of-bounds wide tile");
 
         &self.tiles[y * self.width_tiles() + x]
     }
@@ -70,7 +70,7 @@ impl Wide {
     ///
     /// Panics if the index is out-of-range.
     pub fn get_mut(&mut self, x: usize, y: usize) -> &mut WideTile {
-        assert!(x < self.width && y < self.height);
+        assert!(x < self.width && y < self.height, "attempted to access out-of-bounds wide tile");
 
         let idx = y * self.width_tiles() + x;
         &mut self.tiles[idx]
