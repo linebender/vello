e8240b82b04d671c5796359ebd62e9eb7c487806
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-03-04 20:03:50 +0100

    1

---
+++ sparse_strips/vello_cpu/src/util.rs
@@ -0,0 +1,25 @@
+use peniko::color::{PremulColor, Srgb};
+
+pub(crate) trait ColorExt {
+    /// Using the already-existing `to_rgba8` is slow on x86 because it involves rounding, so
+    /// we use a fast method with just + 0.5.
+    fn to_rgba8_fast(&self) -> [u8; 4];
+}
+
+impl ColorExt for PremulColor<Srgb> {
+    fn to_rgba8_fast(&self) -> [u8; 4] {
+        [
+            (self.components[0] * 255.0 + 0.5) as u8,
+            (self.components[1] * 255.0 + 0.5) as u8,
+            (self.components[2] * 255.0 + 0.5) as u8,
+            (self.components[3] * 255.0 + 0.5) as u8,
+        ]
+    }
+}
+
+pub(crate) mod scalar {
+    #[inline(always)]
+    pub(crate) const fn div_255(val: u16) -> u16 {
+        (val + 1 + (val >> 8)) >> 8
+    }
+}
