c768a99197c0f5cfcd65a6b5e28b2ab36a2edab3
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-03-05 20:19:20 +0100

    Prevent overflow

--- sparse_strips/vello_cpu/src/pixmap.rs
+++ sparse_strips/vello_cpu/src/pixmap.rs
@@ -14,7 +14,7 @@ pub struct Pixmap {
 impl Pixmap {
     /// Create a new pixmap with the given width and height in pixels.
     pub fn new(width: u16, height: u16) -> Self {
-        let buf = vec![0; (width * height * 4) as usize];
+        let buf = vec![0; width as usize * height as usize * 4];
         Self { width, height, buf }
     }
 
