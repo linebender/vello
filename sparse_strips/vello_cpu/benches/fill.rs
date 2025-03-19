ce4e879fe8c2e05fd04830498438d8984e488ea6
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-03-19 16:41:55 +0100

    try to fix CI

--- sparse_strips/vello_cpu/benches/fill.rs
+++ sparse_strips/vello_cpu/benches/fill.rs
@@ -1,3 +1,8 @@
+// Copyright 2025 the Vello Authors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+#![allow(missing_docs, reason = "Not needed for benchmarks")]
+
 use criterion::Criterion;
 use rand::prelude::StdRng;
 use rand::{Rng, SeedableRng};
--- sparse_strips/vello_cpu/benches/main.rs
+++ sparse_strips/vello_cpu/benches/main.rs
@@ -1,3 +1,6 @@
+// Copyright 2025 the Vello Authors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 mod fill;
 
 use criterion::{criterion_group, criterion_main};
