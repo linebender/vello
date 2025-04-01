0bc24ec508f19edae5e633e57cc4024741bc5b13
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-04-01 09:45:37 +0200

    Add more tests

--- sparse_strips/vello_cpu/tests/sweep_gradient.rs
+++ sparse_strips/vello_cpu/tests/sweep_gradient.rs
@@ -1,4 +1,4 @@
-use crate::util::{check_ref, get_ctx, star_path, stops_green_blue};
+use crate::util::{check_ref, get_ctx, star_path, stops_blue_green_red_yellow, stops_green_blue, stops_green_blue_with_alpha};
 use std::f64::consts::PI;
 use vello_common::color::palette::css::{BLACK, BLUE, GREEN, RED, WHITE, YELLOW};
 use vello_common::kurbo::{Affine, Point, Rect};
@@ -6,21 +6,37 @@ use vello_cpu::paint::{LinearGradient, Stop, SweepGradient};
 
 mod util;
 
+macro_rules! basic {
+    ($stops:expr, $name:expr) => {
+        let mut ctx = get_ctx(100, 100, false);
+        let rect = Rect::new(10.0, 10.0, 90.0, 90.0);
+    
+        let gradient = SweepGradient {
+            center: Point::new(50.0, 50.0),
+            start_angle: 0.0,
+            end_angle: 360.0,
+            stops: $stops,
+            extend: vello_common::peniko::Extend::Pad,
+        };
+    
+        ctx.set_paint(gradient);
+        ctx.fill_rect(&rect);
+        
+        check_ref(&ctx, $name);
+    };
+}
+
 #[test]
 fn gradient_sweep_2_stops() {
-    let mut ctx = get_ctx(100, 100, false);
-    let rect = Rect::new(10.0, 10.0, 90.0, 90.0);
+    basic!(stops_green_blue(), "gradient_sweep_2_stops");
+}
 
-    let gradient = SweepGradient {
-        center: Point::new(50.0, 50.0),
-        start_angle: 0.0,
-        end_angle: 360.0,
-        stops: stops_green_blue(),
-        extend: vello_common::peniko::Extend::Pad,
-    };
+#[test]
+fn gradient_sweep_2_stops_with_alpha() {
+    basic!(stops_green_blue_with_alpha(), "gradient_sweep_2_stops_with_alpha");
+}
 
-    ctx.set_paint(gradient);
-    ctx.fill_rect(&rect);
-
-    check_ref(&ctx, "gradient_sweep_2_stops");
+#[test]
+fn gradient_sweep_4_stops() {
+    basic!(stops_blue_green_red_yellow(), "gradient_sweep_4_stops");
 }
