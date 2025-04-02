2952ec03cca43dc7032f7e8f9741c8b2a9df67f1
Alex Gemberg <gemberg@canva.com>
2025-04-02 15:39:21 +1100

    refact(svg.rs): use Affine instead of scale number, pass resizable prop on winit window creationg

--- sparse_strips/vello_hybrid/examples/common/mod.rs
+++ sparse_strips/vello_hybrid/examples/common/mod.rs
@@ -18,30 +18,26 @@ use wgpu::{
 use winit::{event_loop::ActiveEventLoop, window::Window};
 
 /// Define a render function that works with our `pico_svg::Item` type
-pub(crate) fn render_svg(ctx: &mut Scene, scale: f64, items: &[Item]) {
-    fn render_svg_inner(ctx: &mut Scene, items: &[Item], transform: Affine) {
-        ctx.set_transform(transform);
-        for item in items {
-            match item {
-                Item::Fill(fill_item) => {
-                    ctx.set_paint(fill_item.color.into());
-                    ctx.fill_path(&fill_item.path);
-                }
-                Item::Stroke(stroke_item) => {
-                    let style = Stroke::new(stroke_item.width);
-                    ctx.set_stroke(style);
-                    ctx.set_paint(stroke_item.color.into());
-                    ctx.stroke_path(&stroke_item.path);
-                }
-                Item::Group(group_item) => {
-                    render_svg_inner(ctx, &group_item.children, transform * group_item.affine);
-                    ctx.set_transform(transform);
-                }
+pub(crate) fn render_svg(ctx: &mut Scene, items: &[Item], transform: Affine) {
+    ctx.set_transform(transform);
+    for item in items {
+        match item {
+            Item::Fill(fill_item) => {
+                ctx.set_paint(fill_item.color.into());
+                ctx.fill_path(&fill_item.path);
+            }
+            Item::Stroke(stroke_item) => {
+                let style = Stroke::new(stroke_item.width);
+                ctx.set_stroke(style);
+                ctx.set_paint(stroke_item.color.into());
+                ctx.stroke_path(&stroke_item.path);
+            }
+            Item::Group(group_item) => {
+                render_svg(ctx, &group_item.children, transform * group_item.affine);
+                ctx.set_transform(transform);
             }
         }
     }
-
-    render_svg_inner(ctx, items, Affine::scale(scale));
 }
 
 /// Helper function that creates a Winit window and returns it (wrapped in an Arc for sharing)
@@ -50,10 +46,11 @@ pub(crate) fn create_winit_window(
     width: u32,
     height: u32,
     initially_visible: bool,
+    resizable: bool,
 ) -> Arc<Window> {
     let attr = Window::default_attributes()
         .with_inner_size(winit::dpi::PhysicalSize::new(width, height))
-        .with_resizable(false)
+        .with_resizable(resizable)
         .with_title("Vello SVG Renderer")
         .with_visible(initially_visible)
         .with_active(true);
