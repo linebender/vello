c47e6ca604812f80c71feaaa9108e4c1392b1a1e
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-04-22 14:13:22 +0100

    Remove crate contents

--- image_filters/vello_filters_cpu/src/lib.rs
+++ image_filters/vello_filters_cpu/src/lib.rs
@@ -2,18 +2,3 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 //! A library of image filters which run on the CPU.
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
