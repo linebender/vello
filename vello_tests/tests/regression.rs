bb740abb70ac5b0956a4cf0bbbd7d0860dd96a15
sagudev <16504129+sagudev@users.noreply.github.com>
2025-05-06 19:30:23 +0200

    Add test for image roundtrip
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- vello_tests/tests/regression.rs
+++ vello_tests/tests/regression.rs
@@ -3,10 +3,11 @@
 
 //! Tests to ensure that certain issues which don't deserve a test scene don't regress
 
+use scenes::ImageCache;
 use vello::{
     AaConfig, Scene,
     kurbo::{Affine, RoundedRect, Stroke},
-    peniko::color::palette,
+    peniko::{ImageQuality, color::palette},
 };
 use vello_tests::{TestParams, snapshot_test_sync};
 
@@ -20,6 +21,25 @@ fn rounded_rectangle_watertight() {
     scene.stroke(&stroke, Affine::IDENTITY, palette::css::WHITE, None, &rect);
     let mut params = TestParams::new("rounded_rectangle_watertight", 70, 30);
     params.anti_aliasing = AaConfig::Msaa16;
+    snapshot_test_sync(scene, &params)
+        .unwrap()
+        .assert_mean_less_than(0.001);
+}
+
+const DATA_IMAGE_PNG: &[u8] = include_bytes!("./data_image.png");
+
+#[test]
+#[should_panic(expected = "Expected mean to be less than 0.001, got 0.3072978")]
+fn test_data_image_roundtrip() {
+    let mut scene = Scene::new();
+    let mut images = ImageCache::new();
+    let image = images
+        .from_bytes(0, DATA_IMAGE_PNG)
+        .unwrap()
+        .with_quality(ImageQuality::Low);
+    scene.draw_image(&image, Affine::IDENTITY);
+    let mut params = TestParams::new("data_image_roundtrip", image.width, image.height);
+    params.anti_aliasing = AaConfig::Area;
     snapshot_test_sync(scene, &params)
         .unwrap()
         .assert_mean_less_than(0.001);
