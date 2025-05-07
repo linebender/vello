d83b57cb2abadaeb071edacfe82136570a724335
Thomas Churchman <thomas@churchman.nl>
2025-05-07 11:32:06 +0200

    Pixmap PremulRgba8

--- sparse_strips/vello_api/src/pixmap.rs
+++ sparse_strips/vello_api/src/pixmap.rs
@@ -18,32 +18,31 @@ pub struct Pixmap {
     width: u16,
     /// Height of the pixmap in pixels.
     height: u16,
-    /// Buffer of the pixmap in RGBA format.
-    buf: Vec<u8>,
+    /// Buffer of the pixmap in RGBA8 format.
+    buf: Vec<PremulRgba8>,
 }
 
 impl Pixmap {
     /// Create a new pixmap with the given width and height in pixels.
     pub fn new(width: u16, height: u16) -> Self {
-        let buf = vec![0; width as usize * height as usize * 4];
+        let buf = vec![PremulRgba8::from_u32(0); width as usize * height as usize];
         Self { width, height, buf }
     }
 
-    /// Create a new pixmap with the given underlying data interpreted as premultiplied RGBA8.
+    /// Create a new pixmap with the given underlying data premultiplied RGBA8 data.
     ///
-    /// The `data` vector must be of length `width * height * 4` exactly.
+    /// The `data` vector must be of length `width * height` exactly.
     ///
-    /// The pixels are in row-major order. Each pixel consists of four bytes in the order
-    /// `[r, g, b, a]`.
+    /// The pixels are in row-major order.
     ///
     /// # Panics
     ///
-    /// Panics if the `data` vector is not of length `width * height * 4`.
-    pub fn from_parts(data: Vec<u8>, width: u16, height: u16) -> Self {
+    /// Panics if the `data` vector is not of length `width * height`.
+    pub fn from_parts(data: Vec<PremulRgba8>, width: u16, height: u16) -> Self {
         assert_eq!(
             data.len(),
-            usize::from(width) * usize::from(height) * 4,
-            "Expected `data` to have length of exactly `width * height * 4`"
+            usize::from(width) * usize::from(height),
+            "Expected `data` to have length of exactly `width * height`"
         );
         Self {
             width,
@@ -68,8 +67,15 @@ impl Pixmap {
             clippy::cast_possible_truncation,
             reason = "cannot overflow in this case"
         )]
-        for comp in self.data_mut() {
-            *comp = ((alpha as u16 * *comp as u16) / 255) as u8;
+        let multiply = |component| ((alpha as u16 * component as u16) / 255) as u8;
+
+        for pixel in self.data_mut() {
+            *pixel = PremulRgba8 {
+                r: multiply(pixel.r),
+                g: multiply(pixel.g),
+                b: multiply(pixel.b),
+                a: multiply(pixel.a),
+            };
         }
     }
 
@@ -82,87 +88,101 @@ impl Pixmap {
         );
 
         let mut reader = decoder.read_info()?;
-        let mut img_data = vec![0; reader.output_buffer_size()];
-        let info = reader.next_frame(&mut img_data)?;
+        let mut pixmap = {
+            let info = reader.info();
+            let width: u16 = info
+                .width
+                .try_into()
+                .map_err(|_| png::DecodingError::LimitsExceeded)?;
+            let height: u16 = info
+                .height
+                .try_into()
+                .map_err(|_| png::DecodingError::LimitsExceeded)?;
+            Pixmap::new(width, height)
+        };
+
+        // Note `reader.info()` returns the pre-transformation color type output, whereas
+        // `reader.output_color_type()` takes the transformation into account.
+        let (color_type, bit_depth) = reader.output_color_type();
         debug_assert_eq!(
-            info.bit_depth,
+            bit_depth,
             png::BitDepth::Eight,
             "normalize_to_color8 means the bit depth is always 8."
         );
 
-        let decoded_data = match info.color_type {
+        match color_type {
             png::ColorType::Rgb | png::ColorType::Grayscale => {
                 unreachable!("We set a transformation to always convert to alpha")
             }
             png::ColorType::Indexed => {
                 unreachable!("Transformation should have expanded indexed images")
             }
-            png::ColorType::Rgba => img_data,
+            png::ColorType::Rgba => {
+                debug_assert_eq!(pixmap.data_as_u8_slice().len(), reader.output_buffer_size());
+                reader.next_frame(pixmap.data_as_u8_slice_mut())?;
+            }
             png::ColorType::GrayscaleAlpha => {
-                let mut rgba_data = Vec::with_capacity(img_data.len() * 2);
-                for slice in img_data.chunks(2) {
-                    let gray = slice[0];
-                    let alpha = slice[1];
-                    rgba_data.push(gray);
-                    rgba_data.push(gray);
-                    rgba_data.push(gray);
-                    rgba_data.push(alpha);
-                }
+                debug_assert_eq!(pixmap.data().len() * 2, reader.output_buffer_size());
+                let mut grayscale_data = vec![0; reader.output_buffer_size()];
+                reader.next_frame(&mut grayscale_data)?;
 
-                rgba_data
+                for (grayscale_pixel, pixmap_pixel) in
+                    grayscale_data.chunks_exact(2).zip(pixmap.data_mut())
+                {
+                    let [gray, alpha] = grayscale_pixel.try_into().unwrap();
+                    *pixmap_pixel = PremulRgba8 {
+                        r: gray,
+                        g: gray,
+                        b: gray,
+                        a: alpha,
+                    };
+                }
             }
         };
 
-        let premultiplied = decoded_data
-            .chunks_exact(4)
-            .flat_map(|d| {
-                let alpha = d[3] as u16;
-                #[expect(
-                    clippy::cast_possible_truncation,
-                    reason = "Overflow should be impossible."
-                )]
-                let premultiply = |e: u8| ((e as u16 * alpha) / 255) as u8;
+        for pixel in pixmap.data_mut() {
+            let alpha = pixel.a as u16;
+            #[expect(
+                clippy::cast_possible_truncation,
+                reason = "Overflow should be impossible."
+            )]
+            let premultiply = |e: u8| ((e as u16 * alpha) / 255) as u8;
+            pixel.r = premultiply(pixel.r);
+            pixel.g = premultiply(pixel.g);
+            pixel.b = premultiply(pixel.b);
+        }
 
-                if alpha == 0 {
-                    [0, 0, 0, 0]
-                } else {
-                    [
-                        premultiply(d[0]),
-                        premultiply(d[1]),
-                        premultiply(d[2]),
-                        d[3],
-                    ]
-                }
-            })
-            .collect::<Vec<_>>();
+        Ok(pixmap)
+    }
 
-        Ok(Self {
-            width: info
-                .width
-                .try_into()
-                .map_err(|_| png::DecodingError::LimitsExceeded)?,
-            height: info
-                .height
-                .try_into()
-                .map_err(|_| png::DecodingError::LimitsExceeded)?,
-            buf: premultiplied,
-        })
+    /// Returns a reference to the underlying data as premultiplied RGBA8.
+    ///
+    /// The pixels are in row-major order.
+    pub fn data(&self) -> &[PremulRgba8] {
+        &self.buf
+    }
+
+    /// Returns a mutable reference to the underlying data as premultiplied RGBA8.
+    ///
+    /// The pixels are in row-major order.
+    pub fn data_mut(&mut self) -> &mut [PremulRgba8] {
+        &mut self.buf
     }
 
     /// Returns a reference to the underlying data as premultiplied RGBA8.
     ///
     /// The pixels are in row-major order. Each pixel consists of four bytes in the order
     /// `[r, g, b, a]`.
-    pub fn data(&self) -> &[u8] {
-        &self.buf
+    pub fn data_as_u8_slice(&self) -> &[u8] {
+        bytemuck::cast_slice(&self.buf)
     }
 
     /// Returns a mutable reference to the underlying data as premultiplied RGBA8.
     ///
     /// The pixels are in row-major order. Each pixel consists of four bytes in the order
     /// `[r, g, b, a]`.
-    pub fn data_mut(&mut self) -> &mut [u8] {
-        &mut self.buf
+    pub fn data_as_u8_slice_mut(&mut self) -> &mut [u8] {
+        bytemuck::cast_slice_mut(&mut self.buf)
     }
 
     /// Sample a pixel from the pixmap.
@@ -170,19 +190,14 @@ impl Pixmap {
     /// The pixel data is [premultiplied RGBA8][PremulRgba8].
     #[inline(always)]
     pub fn sample(&self, x: u16, y: u16) -> PremulRgba8 {
-        let idx = 4 * (self.width as usize * y as usize + x as usize);
-
-        // The conversion here is a no-op, as `PremulRgba8`'s fields are in the same memory order
-        // as the pixel data.
-        PremulRgba8::from_u8_array(self.buf[idx..][..4].try_into().unwrap())
+        let idx = self.width as usize * y as usize + x as usize;
+        self.buf[idx]
     }
 
-    /// Consume the pixmap, returning the data as the underlying [`Vec`] of premultiplied RGBA8
-    /// bytes.
+    /// Consume the pixmap, returning the data as the underlying [`Vec`] of premultiplied RGBA8.
     ///
-    /// The pixels are in row-major order. Each pixel consists of four bytes in the order
-    /// `[r, g, b, a]`.
-    pub fn take(self) -> Vec<u8> {
+    /// The pixels are in row-major order.
+    pub fn take(self) -> Vec<PremulRgba8> {
         self.buf
     }
 
@@ -192,18 +207,19 @@ impl Pixmap {
     ///
     /// The pixels are in row-major order. Each pixel consists of four bytes in the order
     /// `[r, g, b, a]`.
-    pub fn take_unpremultiplied(mut self) -> Vec<u8> {
-        for rgba in self.buf.chunks_exact_mut(4) {
-            let alpha = 255.0 / rgba[3] as f32;
-
-            #[expect(clippy::cast_possible_truncation, reason = "deliberate quantization")]
-            if rgba[3] != 0 {
-                rgba[0] = (rgba[0] as f32 * alpha + 0.5) as u8;
-                rgba[1] = (rgba[1] as f32 * alpha + 0.5) as u8;
-                rgba[2] = (rgba[2] as f32 * alpha + 0.5) as u8;
-            }
-        }
-
+    pub fn take_unpremultiplied(self) -> Vec<u8> {
         self.buf
+            .into_iter()
+            .flat_map(|PremulRgba8 { r, g, b, a }| {
+                let alpha = 255.0 / a as f32;
+                if a != 0 {
+                    #[expect(clippy::cast_possible_truncation, reason = "deliberate quantization")]
+                    let unpremultiply = |component| (component as f32 * alpha + 0.5) as u8;
+                    [unpremultiply(r), unpremultiply(g), unpremultiply(b), a]
+                } else {
+                    [r, g, b, a]
+                }
+            })
+            .collect()
     }
 }
