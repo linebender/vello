0cd97597a62d9db04b00ee0df8a29cc0d530f069
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-05-08 14:38:09 +0200

    Replace Apple bitmap emojis test

--- sparse_strips/vello_common/src/glyph.rs
+++ sparse_strips/vello_common/src/glyph.rs
@@ -314,7 +314,7 @@ fn prepare_bitmap_glyph<'a>(
     // the font.
     let bearing_y = if bitmap_glyph.bearing_y == 0.0 && bitmaps.format() == Some(BitmapFormat::Sbix)
     {
-        0.0
+        100.0
     } else {
         bitmap_glyph.bearing_y
     };
