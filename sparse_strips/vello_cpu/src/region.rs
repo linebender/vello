566c878a3f6c8d84374b5eb9ef4f225de48f87cc
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-06-03 23:23:37 +0200

    Add copyright notice

--- sparse_strips/vello_cpu/src/region.rs
+++ sparse_strips/vello_cpu/src/region.rs
@@ -1,3 +1,6 @@
+// Copyright 2025 the Vello Authors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 //! Splitting a single mutable buffer into regions that can be accessed concurrently.
 
 use crate::fine::COLOR_COMPONENTS;
