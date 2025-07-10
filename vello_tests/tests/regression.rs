93beb7ccb3902fbb85172ea4cb35ac995977fda4
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-07-10 18:05:03 +0100

    Don't encode empty strokes (#785)
    Fixes https://github.com/linebender/vello/issues/662
    
    I was unable to isolate why this bug originally occurs (making the
    stroke expansion conditional on the width being non-zero appeared to
    work, but artificially increasing the offset didn't change anything), as
    I failed to find the condition mentioned in
    https://github.com/linebender/vello/issues/662#issuecomment-2289723432.
    
    However, I was able to make the fix suggested by Chad in
    https://github.com/linebender/vello/issues/662#issuecomment-2283730287.
    
    This fix wouldn't apply to manually created (or GPU encoded scenes), but
    that's pretty much fine, as we don't really support those.

--- vello_tests/tests/regression.rs
+++ vello_tests/tests/regression.rs
@@ -4,9 +4,10 @@
 //! Tests to ensure that certain issues which don't deserve a test scene don't regress
 
 use scenes::ImageCache;
+use scenes::SimpleText;
 use vello::{
     AaConfig, Scene,
-    kurbo::{Affine, RoundedRect, Stroke},
+    kurbo::{Affine, Rect, RoundedRect, Stroke},
     peniko::{Extend, ImageQuality, color::palette},
 };
 use vello_tests::{TestParams, smoke_snapshot_test_sync, snapshot_test_sync};
@@ -42,6 +43,49 @@ fn test_data_image_roundtrip_extend_pad() {
     let mut params = TestParams::new("data_image_roundtrip", image.width, image.height);
     params.anti_aliasing = AaConfig::Area;
     smoke_snapshot_test_sync(scene, &params)
+        .unwrap()
+        .assert_mean_less_than(0.001);
+}
+
+/// Test created from <https://github.com/linebender/vello/issues/662>
+#[test]
+#[cfg_attr(skip_gpu_tests, ignore)]
+fn stroke_width_zero() {
+    let mut scene = Scene::new();
+    let stroke = Stroke::new(0.0);
+    let rect = Rect::new(10.0, 10.0, 40.0, 40.0);
+    let rect_stroke_color = palette::css::PEACH_PUFF;
+    scene.stroke(&stroke, Affine::IDENTITY, rect_stroke_color, None, &rect);
+    let mut params = TestParams::new("stroke_width_zero", 50, 50);
+    params.anti_aliasing = AaConfig::Msaa16;
+    snapshot_test_sync(scene, &params)
+        .unwrap()
+        .assert_mean_less_than(0.001);
+}
+
+#[test]
+#[cfg_attr(skip_gpu_tests, ignore)]
+#[expect(clippy::cast_possible_truncation, reason = "Test code")]
+fn text_stroke_width_zero() {
+    let font_size = 12.;
+    let mut scene = Scene::new();
+    let mut simple_text = SimpleText::new();
+    simple_text.add_run(
+        &mut scene,
+        None,
+        font_size,
+        palette::css::WHITE,
+        Affine::translate((0., f64::from(font_size))),
+        None,
+        &Stroke::new(0.),
+        "Testing text",
+    );
+    let params = TestParams::new(
+        "text_stroke_width_zero",
+        (font_size * 6.) as _,
+        (font_size * 1.25).ceil() as _,
+    );
+    snapshot_test_sync(scene, &params)
         .unwrap()
         .assert_mean_less_than(0.001);
 }
