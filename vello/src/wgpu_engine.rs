5091f598b7be108261a346bba01b6f8919c95dc7
sagudev <16504129+sagudev@users.noreply.github.com>
2025-07-11 21:54:17 +0200

    trust shaders

--- vello/src/wgpu_engine.rs
+++ vello/src/wgpu_engine.rs
@@ -834,10 +834,12 @@ impl WgpuEngine {
         entries: Vec<wgpu::BindGroupLayoutEntry>,
         cache: Option<&PipelineCache>,
     ) -> WgpuShader {
-        let shader_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        let shader_module = device.create_shader_module_trusted(wgpu::ShaderModuleDescriptor {
             label: Some(label),
             source: wgpu::ShaderSource::Wgsl(wgsl),
-        });
+        },
+        wgpu::ShaderRuntimeChecks::unchecked(),
+        );
         let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
             label: None,
             entries: &entries,
