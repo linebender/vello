b3ab0b6bc111f82597911b9719a2cc1b88bd642c
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-07-16 10:27:35 +0200

    Add comment

--- sparse_strips/vello_cpu/src/fine/mod.rs
+++ sparse_strips/vello_cpu/src/fine/mod.rs
@@ -92,8 +92,11 @@ impl<S: Simd> NumericVec<S> for u8x16<S> {
 #[inline(always)]
 pub(crate) fn f32_to_u8<S: Simd>(val: f32x16<S>) -> u8x16<S> {
     let simd = val.simd;
+    // Note that converting to u32 first using SIMD and then u8
+    // is much faster than converting directly from f32 to u8.
     let converted = simd.cvt_u32_f32x16(val);
 
+    // TODO: Maybe we can also do this using SIMD?
     [
         converted[0] as u8,
         converted[1] as u8,
