1344548b32265888a758dddd7a2653d0c823f436
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-07-18 11:50:29 +0200

    Merge with main

--- sparse_strips/vello_common/src/util.rs
+++ sparse_strips/vello_common/src/util.rs
@@ -3,34 +3,19 @@
 
 //! Utility functions.
 
-use fearless_simd::{Simd, SimdInto, f32x16, u8x16};
+use fearless_simd::{Simd, f32x16, u8x16};
 
 /// Convert f32x16 to u8x16.
 #[inline(always)]
 pub fn f32_to_u8<S: Simd>(val: f32x16<S>) -> u8x16<S> {
     let simd = val.simd;
-    // Note that converting to u32 first using SIMD and then u8
-    // is much faster than converting directly from f32 to u8.
-    let converted = simd.cvt_u32_f32x16(val);
+    let converted = val.cvt_u32().reinterpret_u8();
 
-    // TODO: Maybe we can also do this using SIMD?
-    [
-        converted[0] as u8,
-        converted[1] as u8,
-        converted[2] as u8,
-        converted[3] as u8,
-        converted[4] as u8,
-        converted[5] as u8,
-        converted[6] as u8,
-        converted[7] as u8,
-        converted[8] as u8,
-        converted[9] as u8,
-        converted[10] as u8,
-        converted[11] as u8,
-        converted[12] as u8,
-        converted[13] as u8,
-        converted[14] as u8,
-        converted[15] as u8,
-    ]
-    .simd_into(val.simd)
+    let (x8_1, x8_2) = simd.split_u8x64(converted);
+    let (p1, p2) = simd.split_u8x32(x8_1);
+    let (p3, p4) = simd.split_u8x32(x8_2);
+
+    let uzp1 = simd.unzip_low_u8x16(p1, p2);
+    let uzp2 = simd.unzip_low_u8x16(p3, p4);
+    simd.unzip_low_u8x16(uzp1, uzp2)
 }
