92d37e94eda47cd5918354237df6257a191575e2
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-07-21 11:25:18 +0200

    Completely SIMDify the `u8_to_f32` and `f32_to_u8` methods (#1107)
    Gives me the following speed bump for bicubic image rendering, so it
    definitely helps for `u8_to_f32`:
    
    ```
    fine/image/quality/high_u8_neon
                            time:   [11.801 µs 11.823 µs 11.847 µs]
                            change: [-21.157% -21.009% -20.868%] (p = 0.00 < 0.05)
                            Performance has improved.
    Found 4 outliers among 100 measurements (4.00%)
      3 (3.00%) high mild
      1 (1.00%) high severe
    ```
    
    For `f32_to_u8`, the results are barely noticeable since we already have
    an optimized version on main, but now it at least is fully SIMDified. :)
    ```
    fine/image/quality/medium_u8_neon
                            time:   [3.0515 µs 3.0538 µs 3.0564 µs]
                            change: [-2.1417% -1.9750% -1.8038%] (p = 0.00 < 0.05)
                            Performance has improved.
    Found 7 outliers among 100 measurements (7.00%)
      4 (4.00%) high mild
      3 (3.00%) high severe
    ```
    
    Blocked by https://github.com/linebender/fearless_simd/pull/33

--- sparse_strips/vello_bench/src/strip.rs
+++ sparse_strips/vello_bench/src/strip.rs
@@ -38,7 +38,8 @@ pub fn render_strips(c: &mut Criterion) {
     }
 
     for item in get_data_items() {
-        strip_single!(item, Level::fallback(), "fallback");
+        // Commenting this out by default since SIMD is what we care about most.
+        // strip_single!(item, Level::fallback(), "fallback");
         let simd_level = Level::new();
         if !matches!(simd_level, Level::Fallback(_)) {
             strip_single!(item, simd_level, "simd");
