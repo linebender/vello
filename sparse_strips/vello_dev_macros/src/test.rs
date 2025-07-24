e24368695ba7e545261c0edae10280ed35eea4f3
sagudev <16504129+sagudev@users.noreply.github.com>
2025-07-24 10:47:53 +0200

    Add test for blend clip
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- sparse_strips/vello_dev_macros/src/test.rs
+++ sparse_strips/vello_dev_macros/src/test.rs
@@ -167,6 +167,7 @@ pub(crate) fn vello_test_inner(attr: TokenStream, item: TokenStream) -> TokenStr
             || input_fn_name_str.contains("mask")
             || input_fn_name_str.contains("mix")
             || input_fn_name_str.contains("blurred_rounded_rect")
+            || input_fn_name_str.contains("clip_clear")
     };
 
     let skip_hybrid_webgl = skip_hybrid;
