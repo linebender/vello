bdaef772c51658879f82fc4e262fc3e16f172abf
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-08-15 22:19:10 +0200

    Reformat

--- sparse_strips/vello_cpu/examples/wasm_cpu/src/lib.rs
+++ sparse_strips/vello_cpu/examples/wasm_cpu/src/lib.rs
@@ -73,8 +73,7 @@ impl AppState {
         self.scenes[self.current_scene].render(&mut self.renderer, self.transform);
 
         // Render the current scene with transform
-        self.renderer
-            .render_to_pixmap(&mut self.pixmap);
+        self.renderer.render_to_pixmap(&mut self.pixmap);
         let rgba_bytes = self.pixmap.data_as_u8_slice();
         let image_data = web_sys::ImageData::new_with_u8_clamped_array_and_sh(
             wasm_bindgen::Clamped(rgba_bytes),
