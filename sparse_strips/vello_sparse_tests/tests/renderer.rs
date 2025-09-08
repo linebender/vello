a102744f8e7f1fda19b0402962b5a5346b635543
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-09-08 23:32:30 +0200

    Save

--- sparse_strips/vello_sparse_tests/tests/renderer.rs
+++ sparse_strips/vello_sparse_tests/tests/renderer.rs
@@ -41,10 +41,12 @@ pub(crate) trait Renderer: Sized + GlyphRenderer {
     );
     fn flush(&mut self);
     fn push_clip_layer(&mut self, path: &BezPath);
+    fn push_clip_path(&mut self, path: &BezPath);
     fn push_blend_layer(&mut self, blend_mode: BlendMode);
     fn push_opacity_layer(&mut self, opacity: f32);
     fn push_mask_layer(&mut self, mask: Mask);
     fn pop_layer(&mut self);
+    fn pop_clip_path(&mut self);
     fn set_stroke(&mut self, stroke: Stroke);
     fn set_paint(&mut self, paint: impl Into<PaintType>);
     fn set_paint_transform(&mut self, affine: Affine);
@@ -121,6 +123,10 @@ impl Renderer for RenderContext {
         Self::push_clip_layer(self, path);
     }
 
+    fn push_clip_path(&mut self, path: &BezPath) {
+        Self::push_clip_path(self, path);
+    }
+
     fn push_blend_layer(&mut self, blend_mode: BlendMode) {
         Self::push_blend_layer(self, blend_mode);
     }
@@ -135,6 +141,10 @@ impl Renderer for RenderContext {
 
     fn pop_layer(&mut self) {
         Self::pop_layer(self);
+    }
+
+    fn pop_clip_path(&mut self) {
+        Self::pop_clip_path(self)
     }
 
     fn set_stroke(&mut self, stroke: Stroke) {
@@ -307,6 +317,10 @@ impl Renderer for HybridRenderer {
         self.scene.push_clip_layer(path);
     }
 
+    fn push_clip_path(&mut self, path: &BezPath) {
+        todo!()
+    }
+
     fn push_blend_layer(&mut self, blend_mode: BlendMode) {
         self.scene.push_layer(None, Some(blend_mode), None, None);
     }
@@ -321,6 +335,10 @@ impl Renderer for HybridRenderer {
 
     fn pop_layer(&mut self) {
         self.scene.pop_layer();
+    }
+
+    fn pop_clip_path(&mut self) {
+        todo!()
     }
 
     fn set_stroke(&mut self, stroke: Stroke) {
