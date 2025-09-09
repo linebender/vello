270dca848df13944d0737342b0e5ebec8e5b8388
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-09-09 21:07:59 +0200

    Refactor strip generator

--- sparse_strips/vello_common/src/strip_generator.rs
+++ sparse_strips/vello_common/src/strip_generator.rs
@@ -7,7 +7,7 @@ use crate::fearless_simd::Level;
 use crate::flatten::{FlattenCtx, Line};
 use crate::kurbo::{Affine, PathEl, Stroke};
 use crate::peniko::Fill;
-use crate::strip::{intersect, PathDataOwned, PathDataRef, PathDataMut, Strip};
+use crate::strip::{PathDataMut, PathDataOwned, PathDataRef, Strip, intersect};
 use crate::tile::Tiles;
 use crate::{flatten, strip};
 use alloc::vec::Vec;
@@ -16,13 +16,17 @@ use alloc::vec::Vec;
 #[derive(Debug)]
 pub struct StripGenerator {
     level: Level,
-    global_alphas: Vec<u8>,
-    temp_alphas: Vec<u8>,
     line_buf: Vec<Line>,
     flatten_ctx: FlattenCtx,
     tiles: Tiles,
-    strip_buf1: Vec<Strip>,
-    strip_buf2: Vec<Strip>,
+    /// The alphas "normally" used when rendering paths.
+    main_alpha_buf: Vec<u8>,
+    /// The main strip buffer.
+    main_strip_buf: Vec<Strip>,
+    /// A temporary alpha array used for storing intermediate results when clipping.
+    temp_alpha_buf: Vec<u8>,
+    /// A temporary strip buffer used for storing intermediate results when clipping.
+    temp_strip_buf: Vec<Strip>,
     width: u16,
     height: u16,
 }
@@ -31,13 +35,13 @@ impl StripGenerator {
     /// Create a new strip generator.
     pub fn new(width: u16, height: u16, level: Level) -> Self {
         Self {
-            global_alphas: Vec::new(),
-            temp_alphas: Vec::new(),
+            main_alpha_buf: Vec::new(),
+            temp_alpha_buf: Vec::new(),
             level,
             line_buf: Vec::new(),
             tiles: Tiles::new(level),
-            strip_buf1: Vec::new(),
-            strip_buf2: Vec::new(),
+            main_strip_buf: Vec::new(),
+            temp_strip_buf: Vec::new(),
             flatten_ctx: FlattenCtx::default(),
             width,
             height,
@@ -47,8 +51,8 @@ impl StripGenerator {
     /// Generate the strips for a filled path.
     pub fn generate_filled_path<'a>(
         &'a mut self,
-        path: impl IntoIterator<Item=PathEl>,
-        mut fill_rule: Fill,
+        path: impl IntoIterator<Item = PathEl>,
+        fill_rule: Fill,
         transform: Affine,
         aliasing_threshold: Option<u8>,
         clip_path: Option<PathDataRef<'_>>,
@@ -61,33 +65,10 @@ impl StripGenerator {
             &mut self.line_buf,
             &mut self.flatten_ctx,
         );
-        
-        if let Some(clip_path) = clip_path {
-            self.make_strips(fill_rule, aliasing_threshold, true);
-            
-            let input_path = PathDataRef {
-                strips: &self.strip_buf2,
-                alphas: &self.temp_alphas,
-                fill: fill_rule,
-            };
-            
-            let target = PathDataMut {
-                strips: &mut self.strip_buf1,
-                alphas: &mut self.global_alphas,
-                fill: &mut fill_rule,
-            };
 
-            intersect(
-                self.level,
-                clip_path,
-                input_path,
-                target
-            );
-        }   else {
-            self.make_strips(fill_rule, aliasing_threshold, false);
-        }
+        self.generate_with_clip_path(fill_rule, aliasing_threshold, clip_path);
 
-        func(&self.strip_buf1, &self.global_alphas);
+        func(&self.main_strip_buf, &self.main_alpha_buf);
     }
 
     /// Generate the strips for a stroked path.
@@ -100,8 +81,8 @@ impl StripGenerator {
         clip_path: Option<PathDataRef<'_>>,
         func: impl FnOnce(&'a [Strip]),
     ) {
-        let mut fill_rule = Fill::NonZero;
-        
+        let fill_rule = Fill::NonZero;
+
         flatten::stroke(
             self.level,
             path,
@@ -111,81 +92,85 @@ impl StripGenerator {
             &mut self.flatten_ctx,
         );
 
+        self.generate_with_clip_path(fill_rule, aliasing_threshold, clip_path);
+
+        func(&self.main_strip_buf);
+    }
+
+    fn generate_with_clip_path(
+        &mut self,
+        mut fill_rule: Fill,
+        aliasing_threshold: Option<u8>,
+        clip_path: Option<PathDataRef<'_>>,
+    ) {
         if let Some(clip_path) = clip_path {
             self.make_strips(fill_rule, aliasing_threshold, true);
 
             let input_path = PathDataRef {
-                strips: &self.strip_buf2,
-                alphas: &self.temp_alphas,
+                strips: &self.temp_strip_buf,
+                alphas: &self.temp_alpha_buf,
                 fill: fill_rule,
             };
 
             let target = PathDataMut {
-                strips: &mut self.strip_buf1,
-                alphas: &mut self.global_alphas,
+                strips: &mut self.main_strip_buf,
+                alphas: &mut self.main_alpha_buf,
                 fill: &mut fill_rule,
             };
 
-            intersect(
-                self.level,
-                clip_path,
-                input_path,
-                target
-            );
-        }   else {
+            intersect(self.level, clip_path, input_path, target);
+        } else {
             self.make_strips(fill_rule, aliasing_threshold, false);
         }
-
-        func(&self.strip_buf1);
     }
 
     /// Return a reference to the current alpha buffer of the strip generator.
     pub fn alpha_buf(&self) -> &[u8] {
-        &self.global_alphas
+        &self.main_alpha_buf
     }
 
     /// Extend the alpha buffer with the given alphas.
     pub fn extend_alpha_buf(&mut self, alphas: &[u8]) {
-        self.global_alphas.extend_from_slice(alphas);
+        self.main_alpha_buf.extend_from_slice(alphas);
     }
 
     /// Set the alpha buffer.
     pub fn set_alpha_buf(&mut self, alpha_buf: Vec<u8>) {
-        self.global_alphas = alpha_buf;
+        self.main_alpha_buf = alpha_buf;
     }
 
     /// Take the alpha buffer and set it to an empty one.
     pub fn take_alpha_buf(&mut self) -> Vec<u8> {
-        core::mem::take(&mut self.global_alphas)
+        core::mem::take(&mut self.main_alpha_buf)
     }
 
     /// Swap the alpha buffer with the given one.
     pub fn replace_alpha_buf(&mut self, alphas: Vec<u8>) -> Vec<u8> {
-        core::mem::replace(&mut self.global_alphas, alphas)
+        core::mem::replace(&mut self.main_alpha_buf, alphas)
     }
 
     /// Reset the strip generator.
     pub fn reset(&mut self) {
         self.line_buf.clear();
         self.tiles.reset();
-        self.global_alphas.clear();
-        self.temp_alphas.clear();
-        self.strip_buf1.clear();
-        self.strip_buf2.clear();
+        self.main_alpha_buf.clear();
+        self.temp_alpha_buf.clear();
+        self.main_strip_buf.clear();
+        self.temp_strip_buf.clear();
     }
 
     fn make_strips(&mut self, fill_rule: Fill, aliasing_threshold: Option<u8>, temp: bool) {
         self.tiles
             .make_tiles(&self.line_buf, self.width, self.height);
         self.tiles.sort_tiles();
-        
+
         let (alphas, strip_buf) = if temp {
-            self.temp_alphas.clear();
-            (&mut self.temp_alphas, &mut self.strip_buf2)
-        }   else {
-            (&mut self.global_alphas, &mut self.strip_buf1)
+            self.temp_alpha_buf.clear();
+            (&mut self.temp_alpha_buf, &mut self.temp_strip_buf)
+        } else {
+            (&mut self.main_alpha_buf, &mut self.main_strip_buf)
         };
-        
+
         strip::render(
             self.level,
             &self.tiles,
@@ -203,6 +188,7 @@ mod tests {
     use crate::fearless_simd::Level;
     use crate::kurbo::{Affine, Rect, Shape};
     use crate::peniko::Fill;
+    use crate::strip::Strip;
     use crate::strip_generator::StripGenerator;
 
     #[test]
@@ -219,14 +205,26 @@ mod tests {
             |_, _| {},
         );
 
+        generator.temp_alpha_buf.extend_from_slice(&[0; 16]);
+        generator.temp_strip_buf.push(Strip {
+            x: 0,
+            y: 0,
+            alpha_idx: 0,
+            winding: 0,
+        });
+
         assert!(!generator.line_buf.is_empty());
-        assert!(!generator.strip_buf1.is_empty());
-        assert!(!generator.global_alphas.is_empty());
+        assert!(!generator.main_strip_buf.is_empty());
+        assert!(!generator.temp_strip_buf.is_empty());
+        assert!(!generator.main_alpha_buf.is_empty());
+        assert!(!generator.temp_alpha_buf.is_empty());
 
         generator.reset();
 
         assert!(generator.line_buf.is_empty());
-        assert!(generator.strip_buf1.is_empty());
-        assert!(generator.global_alphas.is_empty());
+        assert!(generator.main_strip_buf.is_empty());
+        assert!(generator.temp_strip_buf.is_empty());
+        assert!(generator.main_alpha_buf.is_empty());
+        assert!(generator.main_alpha_buf.is_empty());
     }
 }
