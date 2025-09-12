a73fc49e6679222fd9a6e8b9767507bb7c91d9b2
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-09-12 16:14:02 +0200

    Remove copyright notice

--- sparse_strips/vello_common/src/sixth_root.rs
+++ sparse_strips/vello_common/src/sixth_root.rs
@@ -1,19 +1,6 @@
 // Copyright 2025 the Vello Authors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-/*
-   Copyright 2024 Radzivon Bartoshyk
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
 /// Optional FMA, if it is available hardware FMA will use, if not then just scalar `c + a * b`
 #[inline(always)]
 pub(crate) fn fmla(a: f64, b: f64, c: f64) -> f64 {
