9c9b404d17b0e8e719e933568ea11e814a8b5fc9
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-12-05 15:53:14 +0000

    Add high-level docs for textures, etc.

--- sparse_strips/vello_api/src/texture.rs
+++ sparse_strips/vello_api/src/texture.rs
@@ -1,7 +1,41 @@
 // Copyright 2025 the Vello Authors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Texture types used in Vello API.
+//! Types and utilities for textures, which are 2d images owned by a [`Renderer`].
+//!
+//! Most textures in Vello Hybrid are reference counted handles, and stored as [`TextureHandle`].
+//! As the data referred to by owned by the `TextureHandle`s are "renderer-specific".
+//! This means that it can only be used with [`PaintScene`][crate::PaintScene]s which are
+//! associated with that renderer.
+//! This is validated automatically at scene construction time.
+//!
+//! You can create a texture using [`Renderer::create_texture`]; the underlying texture's
+//! content/memory region will be owned by the renderer.
+//! These textures can be used in a few ways, depending on the [`TextureUsages`] they are created with.
+//! These are documented on the associated constants of `TextureUsages`.
+//!
+//! Certain renderers may also support creating textures from external content.
+//! For example, [`VelloHybrid::add_external_texture`][todo] allows creating textures
+//! corresponding to a `wgpu::TextureView`.
+//! This can be used to composite in external content, or for rendering directly to a `Surface`
+//! (i.e. the contents of an operating system window) with Vello Hybrid.
+//! These methods can be accessed through downcasting the renderer to the specific implementation.
+//! Similar mechanisms are also used to provide access to a texture's current content on the CPU.
+//!
+//! We expect Vello API to gain support for scheduling "downloads" of a texture's content, but
+//! this isn't currently implemented.
+//! It is currently not supported in general to transfer textures from one renderer
+//! to another renderer.
+//! This download mechanism *may* allow that in the future, although this may be impossible
+//! for some textures due to their `TextureUsages`.
+//!
+//! It is possible for advanced users to manually allocate and release textures without creating a handle,
+//! by calling to [`Renderer::alloc_untracked_texture`] and [`Renderer::free_untracked_texture`].
+//! This is not recommended for most users, as it will likely requires a lot more bookkeeping on your side.
+//! Additionally, we don't expect textures to be created regularly enough for the allocation cost
+//! of this handle to be significant.
+//! The ids returned from `alloc_untracked_texture` are renderer specific, in the same way as `TextureHandle`s are.
+//! However, this will not be validated at scene construction time (but incorrect usage might lead to future crashes).
 
 use alloc::sync::Arc;
 use bitflags::bitflags;
@@ -40,10 +74,10 @@ bitflags! {
         const RENDER_TARGET = 1 << 0;
         /// This texture can be the target of an "upload image" operation.
         const UPLOAD_TARGET = 1 << 1;
-        /// This texture can be the source for a "download" operation.
-        const DOWNLOAD_SRC = 1 << 2;
         /// This texture (or a subset of it) can be used for painting.
-        const TEXTURE_BINDING = 1 << 3;
+        const TEXTURE_BINDING = 1 << 2;
+        // /// This texture can be the source for a "download" operation.
+        // const DOWNLOAD_SRC = 1 << 3;
         // TODO: Does this make sense to support/require this?
         // /// A subset of this texture can be rendered to.
         // const PARTIAL_RENDER_TARGET = 1<<4;
@@ -52,8 +86,6 @@ bitflags! {
         const SURFACE = Self::RENDER_TARGET.bits();
         /// The usages for an uploaded texture.
         const UPLOAD = Self::UPLOAD_TARGET.bits() | Self::TEXTURE_BINDING.bits();
-        /// The usages for a texture which we want to queue for download.
-        const DOWNLOAD = Self::RENDER_TARGET.bits() | Self::DOWNLOAD_SRC.bits();
     }
 }
 
@@ -67,7 +99,8 @@ impl TextureHandle {
     pub fn id(&self) -> TextureId {
         self.inner.id
     }
-
+    // TODO: Consider making not `pub(crate)`. We need to be careful that people don't create
+    // multiple of these for the same texture accidentally.
     pub(crate) fn new(renderer: Arc<dyn Renderer>, id: TextureId) -> Self {
         Self {
             inner: Arc::new(TextureInner { renderer, id }),
@@ -96,7 +129,7 @@ impl Drop for TextureInner {
     fn drop(&mut self) {
         // TODO: Maybe just log?
         self.renderer
-            .free_texture_raw(self.id)
+            .free_untracked_texture(self.id)
             .expect("Vello API texture managed as a `Texture` shouldn't have been manually freed.");
     }
 }
