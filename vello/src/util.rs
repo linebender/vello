75574ad48b980c2d1932dc47d972c9849bdf0cb1
khayzz13 <khayzz13@users.noreply.github.com>
2026-01-14 22:32:03 -0700

    Add depth_slice field for wgpu 27 compatibility

--- vello/src/util.rs
+++ vello/src/util.rs
@@ -348,6 +348,7 @@ impl BlitterWithLoadOp {
                     load: load_op,
                     store: wgpu::StoreOp::Store,
                 },
+                depth_slice: None,
             })],
             depth_stencil_attachment: None,
             timestamp_writes: None,
