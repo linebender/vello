57f05a1e9bbefef4689025f596b064c8f049ade6
Laurenz Stampfl <laurenz@canva.com>
2026-02-13 09:31:32 +0100

    copyright headers

--- sparse_strips/vello_sparse_tests/tests/unresolved_issues.rs
+++ sparse_strips/vello_sparse_tests/tests/unresolved_issues.rs
@@ -1,3 +1,6 @@
+// Copyright 2026 the Vello Authors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 //! All tests here should work but currently don't. The module is not included in
 //! `mod.rs`, so the tests will never be run.
 
