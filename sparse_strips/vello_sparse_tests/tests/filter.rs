299c520451108bef68d0dc9099ac4eb96a856002
Laurenz Stampfl <laurenz@canva.com>
2026-03-03 15:01:36 +0100

    Add another test

--- sparse_strips/vello_sparse_tests/tests/filter.rs
+++ sparse_strips/vello_sparse_tests/tests/filter.rs
@@ -1025,6 +1025,22 @@ fn filter_extreme_blur(ctx: &mut impl Renderer) {
     ctx.pop_layer();
 }
 
+// We have this test because unlike the first test case, this test has 5 decimations (instead of
+// 4), and having an uneven number of decimations triggers a specal case in vello_hybrid).
+#[vello_test(skip_multithreaded, hybrid_tolerance = 3, width = 400, height = 400)]
+fn filter_extreme_blur_2(ctx: &mut impl Renderer) {
+    let filter = Filter::from_primitive(FilterPrimitive::GaussianBlur {
+        std_deviation: 36.0,
+        edge_mode: EdgeMode::None,
+    });
+    let rect = Rect::new(100.0, 100.0, 300.0, 300.0).to_path(0.1);
+
+    ctx.push_filter_layer(filter);
+    ctx.set_paint(REBECCA_PURPLE);
+    ctx.fill_path(&rect);
+    ctx.pop_layer();
+}
+
 /// Test filter on semi-transparent shapes.
 #[vello_test(skip_multithreaded)]
 fn filter_transparent_shapes(ctx: &mut impl Renderer) {
