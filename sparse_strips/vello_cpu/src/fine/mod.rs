958cd19aea5bf68ed559a76f2ff30fbb56dd7ea0
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2026-05-11 11:39:42 +0200

    sparse_strips: Use `store_slice` wherever possible (#1616)
    A while ago, we added the `store_slice` method to fearless_simd since we
    realized that using `copy_from_slice` doesn't always turn into efficient
    code on x86. Therefore, using this code in vello should lead to better
    performance, at the very least not regress it. I measured this on NEON
    and I'm not seeing any regressions.

--- sparse_strips/vello_cpu/src/fine/mod.rs
+++ sparse_strips/vello_cpu/src/fine/mod.rs
@@ -1073,7 +1073,7 @@ mod macros {
                         for chunk in buf.chunks_exact_mut(16) {
                             let next = self.next().unwrap();
                             let converted = u8x16::<S>::from_f32(next.simd, next);
-                            chunk.copy_from_slice(converted.as_slice());
+                            converted.store_slice(chunk);
                         }
                     })
                 }
@@ -1082,7 +1082,7 @@ mod macros {
                     self.simd.vectorize(#[inline(always)] || {
                         for chunk in buf.chunks_exact_mut(16) {
                             let next = self.next().unwrap();
-                            chunk.copy_from_slice(next.as_slice());
+                            next.store_slice(chunk);
                         }
                     })
                 }
@@ -1101,7 +1101,7 @@ mod macros {
                     self.simd.vectorize(#[inline(always)] || {
                         for chunk in buf.chunks_exact_mut(16) {
                             let next = self.next().unwrap();
-                            chunk.copy_from_slice(next.as_slice());
+                            next.store_slice(chunk);
                         }
                     })
                 }
@@ -1114,7 +1114,7 @@ mod macros {
                         for chunk in buf.chunks_exact_mut(16) {
                             let next = self.next().unwrap();
                             let converted = f32x16::<S>::from_u8(next.simd, next);
-                            chunk.copy_from_slice(converted.as_slice());
+                            converted.store_slice(chunk);
                         }
                     })
                 }
