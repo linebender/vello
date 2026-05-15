4ca1c9876332f6cc602b8c56802d54d175d886d2
Nico Burns <nico@nicoburns.com>
2026-05-15 12:02:10 +0100

    [examples/benches]: Upgrade Parley to v0.9.0 (#1646)
    This removes 2 duplicate versions of Fontations from the crate graph
    which should help a bit with CI compile times.
    
    ---------
    
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- sparse_strips/vello_example_scenes/src/text.rs
+++ sparse_strips/vello_example_scenes/src/text.rs
@@ -83,7 +83,7 @@ impl TextScene {
         };
 
         let mut builder = layout_cx.ranged_builder(&mut font_cx, text, 1.0, true);
-        builder.push_default(FontFamily::parse("Roboto").unwrap());
+        builder.push_default(FontFamily::named("Roboto"));
         builder.push_default(StyleProperty::LineHeight(
             parley::LineHeight::FontSizeRelative(1.3),
         ));
@@ -92,7 +92,7 @@ impl TextScene {
         let mut layout: Layout<ColorBrush> = builder.build(text);
         let max_advance = Some(400.0);
         layout.break_all_lines(max_advance);
-        layout.align(max_advance, Alignment::Middle, AlignmentOptions::default());
+        layout.align(Alignment::Center, AlignmentOptions::default());
 
         Self { layout }
     }
@@ -132,7 +132,7 @@ fn render_glyph_run<T: RenderingContext>(
         run_x += glyph.advance;
 
         Glyph {
-            id: glyph.id as u32,
+            id: glyph.id,
             x: glyph_x,
             y: glyph_y,
         }
