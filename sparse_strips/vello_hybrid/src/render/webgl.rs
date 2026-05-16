2135afb19ef77a58dd403592bbff5ec4de7a57e8
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2026-05-16 15:49:46 +0200

    sparse_strips: Make tile size a const generic instead of an associated constant

--- sparse_strips/vello_hybrid/src/render/webgl.rs
+++ sparse_strips/vello_hybrid/src/render/webgl.rs
@@ -22,7 +22,7 @@ only break in edge cases, and some of them are also only related to conversions 
 
 use crate::render::common::IMAGE_PADDING;
 use crate::{
-    GpuStrip, RenderError, RenderSettings, RenderSize, Resources,
+    GpuStrip, RenderError, RenderSettings, RenderSize, Resources, Tile,
     filter::{FilterContext, FilterInstanceData, FilterPassState, FilterPassTarget},
     gradient_cache::GradientRampCache,
     render::{
@@ -64,7 +64,6 @@ use vello_common::{
     paint::{ImageId, ImageSource},
     peniko::{self},
     pixmap::Pixmap,
-    tile::Tile,
 };
 use vello_sparse_shaders::{clear_slots, filters, render_strips};
 use web_sys::wasm_bindgen::{JsCast, JsValue};
