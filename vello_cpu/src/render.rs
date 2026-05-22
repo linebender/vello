8d8918d9e5223ae2ea77b8b1c62feffeebfc2fac
Fangdun Tsai <fundon@pindash.io>
2026-05-22 18:03:25 +0800

    vello_cpu: add text feature

--- vello_cpu/src/render.rs
+++ vello_cpu/src/render.rs
@@ -9,9 +9,8 @@ use crate::dispatch::Dispatcher;
 use crate::dispatch::multi_threaded::MultiThreadedDispatcher;
 #[cfg(feature = "text")]
 use crate::text::{GlyphAtlasResources, GlyphRunBuilder};
-use glifo::GlyphAtlas;
 #[cfg(feature = "text")]
-use glifo::GlyphPrepCache;
+use glifo::{GlyphAtlas, GlyphPrepCache};
 
 #[cfg(feature = "text")]
 use vello_common::image_cache::ImageCache;
@@ -105,6 +104,7 @@ impl Resources {
     }
 
     /// Returns atlas pages.
+    #[cfg(feature = "text")]
     pub fn pixmaps(&self) -> Option<Vec<Arc<Pixmap>>> {
         self.glyph_resources
             .as_ref()
@@ -112,6 +112,7 @@ impl Resources {
     }
 
     /// Returns glyph atlas cache data.
+    #[cfg(feature = "text")]
     pub fn glyph_atlas_mut(&mut self) -> Option<&mut GlyphAtlas> {
         self.glyph_resources
             .as_mut()
