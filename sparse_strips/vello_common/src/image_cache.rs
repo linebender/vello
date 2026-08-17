6c5fefa5c8f5899104540e0b6aa2209a94e8cc16
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2026-08-17 05:05:30 +0000

    vello_common: Remove the `allocate_excluding` API (#1825)
    This was needed in the past when filter layers were rendered into the
    image atlas. Now that we have our new layer scheduler, I don't think we
    are going to need this anymore. So I propose removing it to simplify the
    code.

--- sparse_strips/vello_common/src/image_cache.rs
+++ sparse_strips/vello_common/src/image_cache.rs
@@ -92,23 +92,15 @@ impl ImageCache {
     }
 
     /// Allocate an image in the cache, with optional transparent padding.
+    #[expect(
+        clippy::cast_possible_truncation,
+        reason = "u16 is enough for the offset and width/height"
+    )]
     pub fn allocate(
         &mut self,
         width: u16,
         height: u16,
         padding: u16,
-    ) -> Result<ImageId, AtlasError> {
-        self.allocate_excluding(width, height, padding, None)
-    }
-
-    /// Allocate an image in the cache, with optional transparency padding
-    /// and optionally excluding a specific atlas.
-    pub fn allocate_excluding(
-        &mut self,
-        width: u16,
-        height: u16,
-        padding: u16,
-        exclude_atlas_id: Option<AtlasId>,
     ) -> Result<ImageId, AtlasError> {
         let doubled_padding = u32::from(padding) * 2;
         let padded_width = u32::from(width) + doubled_padding;
@@ -124,11 +116,9 @@ impl ImageCache {
                 max_height,
             });
         };
-        let atlas_alloc = self.atlas_manager.try_allocate_excluding(
-            padded_width,
-            padded_height,
-            exclude_atlas_id,
-        )?;
+        let atlas_alloc = self
+            .atlas_manager
+            .try_allocate(padded_width, padded_height)?;
 
         let slot_idx = self.free_idxs.pop().unwrap_or_else(|| {
             // No free slots, append to vector
