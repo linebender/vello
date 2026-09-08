fbea52832eae7d14c03e7b4dd4cbc9c7df42a09b
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2026-09-08 14:29:25 +0200

    fix wasm

--- vello_gpu/examples/winit/src/main.rs
+++ vello_gpu/examples/winit/src/main.rs
@@ -401,7 +401,7 @@ impl ApplicationHandler for App<'_> {
                         &texture_view,
                         Some(depth_texture_view),
                         &texture_bindings,
-                        vello_hybrid::TargetInit::Clear(vello_hybrid::ClearSettings::default()),
+                        vello_gpu::TargetInit::Clear(vello_gpu::ClearSettings::default()),
                     )
                     .unwrap();
 
